c1e74ec91be19bed73f8d9808e97f057101a9fd1
mpalmeos <maie.palmeos@itcollege.ee>
2017-01-27 10:15:35 +0200

    Integer from String!

--- JavaFX/Joon.java
+++ JavaFX/Joon.java
@@ -5,6 +5,8 @@ package JavaFX;
  */
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -22,17 +24,6 @@ public class Joon extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        int kustX;
-        int kustY;
-        int kuhuX;
-        int kuhuY;
-
-        Line joon = new Line();
-        joon.setStartX(kustX);
-        joon.setStartY(kustY);
-        joon.setEndX(kuhuX);
-        joon.setEndY(kuhuY);
-
         VBox kyssa = new VBox();
 
         TextField KustX = new TextField("Alguse X");
@@ -40,21 +31,33 @@ public class Joon extends Application {
         TextField KuhuX = new TextField("Lõpu X");
         TextField KuhuY = new TextField("Lõpu Y");
 
-        kyssa.getChildren().addAll();
-
-        Pane park = new Pane();
-        park.getChildren().addAll(joon);
-        Scene aken = new Scene(kyssa, 500, 500);
-        Scene aken2 = new Scene(park, 500, 500);
-
         Button nupp = new Button("Sisesta");
-        nupp.setOnAction(event -> {
-            primaryStage.setScene(aken2);
-            primaryStage.show();
-        });
+
+        kyssa.getChildren().addAll(KustX, KustY, KuhuX, KuhuY, nupp);
+
+        Scene aken = new Scene(kyssa, 500, 500);
 
         primaryStage.setScene(aken);
         primaryStage.show();
 
+        nupp.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+                int kustX = Integer.parseInt(KustX.getText());
+                int kustY = Integer.parseInt(KustY.getText());
+                int kuhuX = Integer.parseInt(KuhuX.getText());
+                int kuhuY = Integer.parseInt(KuhuY.getText());
+
+                Line joon = new Line(kustX, kustY, kuhuX, kuhuY);
+                Pane park = new Pane();
+                park.getChildren().addAll(joon);
+
+                Scene aken2 = new Scene(park, 500, 500);
+
+                primaryStage.setScene(aken2);
+                primaryStage.show();
+            }
+        });
     }
 }
